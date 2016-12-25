fb44d737bea271abd39766b132e25357c75f3587
DESKTOP-1NI0VI2\Elitez <visu.rmk@gmail.com>
2016-12-25 10:30:25 +0530

    mongodb connection added

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://vis:vis@ds145138.mlab.com:45138/shopmanager', ['customers']);
 
+//Fetching All Customers
 router.get('/customers', function(req, res, next){
-    res.send('CUSTOMER API');
+    db.customers.find(function(err, customers){
+        if(err){
+            res.send(err);
+        }
+        res.json(customers);
+    })
 })
 
 module.exports = router;
