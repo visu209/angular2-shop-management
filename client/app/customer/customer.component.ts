a2c957675aa71425ba0d103cdd3a0ccb3c7a8a46
Viswam <viswam.ramakichenane@cognizant.com>
2016-12-27 19:19:25 +0530

    Customer Module Added

---
+++ client/app/customer/customer.component.ts
@@ -0,0 +1,12 @@
+import {Component} from  '@angular/core';
+import {Customer} from './customer';
+
+@Component({
+    moduleId: module.id,
+    selector: 'customers',
+    templateUrl: 'customer.component.html'
+})
+
+export class CustomerComponent{
+    
+}
---
+++ client/app/customer/customer.ts
@@ -0,0 +1,13 @@
+export class Customer{
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public phone: number,
+        public address1: string,
+        public address2: string,
+        public area: string,
+        public city: string,
+        public zip: string,
+        public email: string
+    ){}
+}
