c77c5fdfbf01ace8683ec0cfa06d878c65aa7728
DESKTOP-1NI0VI2\Elitez <visu.rmk@gmail.com>
2016-12-28 08:58:05 +0530

    Customer Component Activated

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -11,12 +11,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            template: "<customers></customers>",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- client/app/customer/customer.component.js
+++ client/app/customer/customer.component.js
@@ -9,21 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app.component');
-var customer_component_1 = require('./customer/customer.component');
-var AppModule = (function () {
-    function AppModule() {
+var CustomerComponent = (function () {
+    function CustomerComponent() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent, customer_component_1.CustomerComponent],
-            bootstrap: [app_component_1.AppComponent]
+    CustomerComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'customers',
+            templateUrl: 'customer.component.html'
         }), 
         __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    ], CustomerComponent);
+    return CustomerComponent;
 }());
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+exports.CustomerComponent = CustomerComponent;
+//# sourceMappingURL=customer.component.js.map
