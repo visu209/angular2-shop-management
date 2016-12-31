926bf324a48f256586a2442db3bfb08c6cd5d8f1
DESKTOP-1NI0VI2\Elitez <visu.rmk@gmail.com>
2016-12-31 08:09:37 +0530

    Service URL modified to heroku path

--- client/app/services/customer.service.ts
+++ client/app/services/customer.service.ts
@@ -4,12 +4,13 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CustomerService {
+    port: 'process.env.PORT || 3000';
     constructor(private http: Http) {
         console.log("customer service initialized...");
     }
 
     getCustomers() {
-        return this.http.get('http://localhost:3000/api/customers')
+        return this.http.get('http://immense-island-54112.herokuapp.com/api/customers')
             .map(res => res.json());
     }
 }
